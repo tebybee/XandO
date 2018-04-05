4960a6744d5b5bbbf71ff7bff39efe830ce7fe57
tebybee <tbybee@mail.csuchico.edu>
2018-04-05 13:14:42 -0700

    Plays the game tic-tac-toe and records if there is a winner.

--- main.cpp
+++ main.cpp
@@ -78,48 +78,48 @@ int main()
                 //checks whether the rows have a winner
                 if((board[0][0] == board[0][1]) && (board[0][1] == board[0][2]) && (board[0][0] != BLANK)) 
                 {   
-                    cout<<"Winner is "<<board[0][0]<< " \n";
+                    cout<<"Winner is "<<board[0][0]<< "\n";
                     playing = false;
                 }
                 else if((board[1][0] == board[1][1]) && (board[1][1] == board[1][2]) && (board[1][0] != BLANK))
                 {
    
-                    cout<<"Winner is "<<board[1][0]<< " \n";
+                    cout<<"Winner is "<<board[1][0]<< "\n";
                     playing = false;
                 }
                 else if((board[2][0] == board[2][1]) && (board[2][1] == board[2][2]) && (board[2][0] != BLANK)) 
                 {   
-                    cout<<"Winner is "<<board[2][0]<< " \n";
+                    cout<<"Winner is "<<board[2][0]<< "\n";
                     playing = false;
                 }
                 //checks whether the columns have a winner.
                 else if((board[0][0] == board[1][0]) && (board[1][0] == board[2][0]) && (board[0][0] != BLANK))
                 {
    
-                    cout<<"Winner is "<<board[0][0]<< " \n";
+                    cout<<"Winner is "<<board[0][0]<< "\n";
                     playing = false;
                 }
                 else if((board[1][0] == board[1][1]) && (board[1][1] == board[1][2]) && (board[1][0] != BLANK)) 
                 {   
-                    cout<<"Winner is "<<board[1][0]<< " \n";
+                    cout<<"Winner is "<<board[1][0]<< "\n";
                     playing = false;
                 }
                 else if((board[2][0] == board[2][1]) && (board[2][1] == board[2][2]) && (board[2][0] != BLANK))
                 {
    
-                    cout<<"Winner is "<<board[2][0]<< " \n"; 
+                    cout<<"Winner is "<<board[2][0]<< "\n"; 
                     playing = false;
                 }
                 //checks whether the diagnals have a winner.
                 else if((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && (board[0][0] != BLANK)) 
                 {   
-                    cout<<"Winner is "<<board[0][0]<< " \n";
+                    cout<<"Winner is "<<board[0][0]<< "\n";
                     playing = false;
                 }
                 else if((board[2][0] == board[1][1]) && (board[1][1] == board[0][2]) && (board[2][0] != BLANK))
                 {
    
-                    cout<<"Winner is "<<board[2][0]<< " \n";
+                    cout<<"Winner is "<<board[2][0]<< "\n";
                     playing = false;
                 }
 
